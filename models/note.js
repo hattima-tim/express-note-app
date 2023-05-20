77708ce7f5ea1cfa800c90816f5dfb03e221fe04
hattima-tim <irfanridwan20@gmail.com>
2023-05-20 08:13:13 +0000

    create Note model

---
+++ models/note.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const NoteSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  details: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  catagory: {
+    type: Schema.Types.ObjectId,
+    ref: "Catagory",
+    required: true,
+  },
+});
+
+NoteSchema.virtual("url").get(function () {
+  return `/catagory/notes/${this._id}`;
+});
+
+module.exports = mongoose.model("Note", NoteSchema);
