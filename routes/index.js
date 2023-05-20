c8724729df373e9a724e1817bd64342d423c90d1
hattima-tim <irfanridwan20@gmail.com>
2023-05-20 15:30:08 +0000

    add index controller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const home_controller=require('../controllers/homeController')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', home_controller.index);
 
 module.exports = router;
