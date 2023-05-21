f683f1c6453c2c56e1fd9a1b69f105624c9c016f
hattima-tim <irfanridwan20@gmail.com>
2023-05-21 07:03:20 +0000

    create route for note creation

--- routes/category.js
+++ routes/category.js
@@ -4,5 +4,6 @@ const category_controller = require("../controllers/categoryController");
 
 /* GET home page. */
 router.get("/:id", category_controller.note_list);
+router.get('/:id/create',category_controller.note_create_get)
 
 module.exports = router;
