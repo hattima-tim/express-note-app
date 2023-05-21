c514b90a0e07409df9e4a05e68027d4cd2294469
hattima-tim <irfanridwan20@gmail.com>
2023-05-21 16:00:18 +0000

    add route for note creating post request

--- routes/category.js
+++ routes/category.js
@@ -4,6 +4,7 @@ const category_controller = require("../controllers/categoryController");
 
 /* GET home page. */
 router.get("/:id", category_controller.note_list);
-router.get('/:id/create',category_controller.note_create_get)
+router.get("/:id/create", category_controller.note_create_get);
+router.post("/:id/create", category_controller.note_create_post);
 
 module.exports = router;
