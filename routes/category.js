f7c24c123bdc071756d0228840f3a355e613d1f4
hattima-tim <irfanridwan20@gmail.com>
2023-05-22 08:18:46 +0000

    add post route for updating notes

--- routes/category.js
+++ routes/category.js
@@ -8,5 +8,6 @@ router.get("/:id/create", category_controller.note_create_get);
 router.post("/:id/create", category_controller.note_create_post);
 router.get('/:categoryId/note/:noteId',category_controller.note_detail)
 router.get('/:categoryId/note/:noteId/update',category_controller.note_update_get)
+router.post("/:categoryId/note/:noteId/update",category_controller.note_update_post);
 
 module.exports = router;
