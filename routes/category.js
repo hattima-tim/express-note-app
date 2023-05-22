70058c7795b269ee1e695e856ad7fc77ea57d186
hattima-tim <irfanridwan20@gmail.com>
2023-05-22 15:39:48 +0000

    add route for note deletion

--- routes/category.js
+++ routes/category.js
@@ -4,10 +4,26 @@ const category_controller = require("../controllers/categoryController");
 
 /* GET home page. */
 router.get("/:id", category_controller.note_list);
+
 router.get("/:id/create", category_controller.note_create_get);
+
 router.post("/:id/create", category_controller.note_create_post);
-router.get('/:categoryId/note/:noteId',category_controller.note_detail)
-router.get('/:categoryId/note/:noteId/update',category_controller.note_update_get)
-router.post("/:categoryId/note/:noteId/update",category_controller.note_update_post);
+
+router.get("/:categoryId/note/:noteId", category_controller.note_detail);
+
+router.get(
+  "/:categoryId/note/:noteId/update",
+  category_controller.note_update_get
+);
+
+router.post(
+  "/:categoryId/note/:noteId/update",
+  category_controller.note_update_post
+);
+
+router.post(
+  "/:categoryId/note/:noteId/delete",
+  category_controller.note_delete_post
+);
 
 module.exports = router;
