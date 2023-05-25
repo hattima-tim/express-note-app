f835e4115baf65e2c98cb69faf0333d47e568fc2
hattima-tim <irfanridwan20@gmail.com>
2023-05-25 17:27:11 +0000

    add route for login

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const home_controller=require('../controllers/homeController')
 /* GET home page. */
 router.get('/', home_controller.index);
 router.get("/sign-up", home_controller.sign_up_get);
+router.get("/log-in", home_controller.log_in_get);
 
 module.exports = router;
