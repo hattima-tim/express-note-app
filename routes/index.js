6932118894e875040e09aa07bda6e6e8209bc710
hattima-tim <irfanridwan20@gmail.com>
2023-05-25 17:26:51 +0000

    add route for sign up

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const home_controller=require('../controllers/homeController')
 
 /* GET home page. */
 router.get('/', home_controller.index);
+router.get("/sign-up", home_controller.sign_up_get);
 
 module.exports = router;
