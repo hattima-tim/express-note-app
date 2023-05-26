050e7e6b41202fa3ff5d5ebda687aceacd86f672
hattima-tim <irfanridwan20@gmail.com>
2023-05-26 04:37:02 +0000

    create post route for log out

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ router.get("/sign-up", home_controller.sign_up_get);
 router.get("/log-in", home_controller.log_in_get);
 router.post("/sign-up", home_controller.sign_up_post);
 router.post("/log-in", home_controller.log_in_post);
+router.post('/log-out',home_controller.log_out)
 
 module.exports = router;
