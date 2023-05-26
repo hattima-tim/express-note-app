8ddff65f0a6ee0e4a16350a8d6d5cad5e617c741
hattima-tim <irfanridwan20@gmail.com>
2023-05-26 04:33:48 +0000

    create post route for log in

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const home_controller=require('../controllers/homeController')
+const home_controller = require("../controllers/homeController");
 
 /* GET home page. */
-router.get('/', home_controller.index);
+router.get("/", home_controller.index);
 router.get("/sign-up", home_controller.sign_up_get);
 router.get("/log-in", home_controller.log_in_get);
-router.post('/sign-up',home_controller.sign_up_post);
+router.post("/sign-up", home_controller.sign_up_post);
+router.post("/log-in", home_controller.log_in_post);
 
 module.exports = router;
